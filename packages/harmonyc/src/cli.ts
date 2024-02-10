11bf2c5d1387d3170b4111da977ebca6ac028b2f
Bernát Kalló <bernat@harmony.ac>
2024-02-10 22:21:46 +0100

    display usage for --help

--- packages/harmonyc/src/cli.ts
+++ packages/harmonyc/src/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { compileFiles } from "./compiler";
 
-if (process.argv.length <3) {
+if (process.argv.length <3 || process.argv.includes('--help')) {
   console.error("Usage: harmonyc <input files glob pattern>")
   process.exit(1)
 }
