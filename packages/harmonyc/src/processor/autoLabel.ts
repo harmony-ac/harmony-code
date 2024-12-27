93769f7ed9524d182e372e4891db016caec773f8
Bernát Kalló <kallo.bernat@gmail.com>
2024-12-27 17:45:09 +0100

    fix: fix autoLabel edge cases

--- packages/harmonyc/src/processor/autoLabel.ts
+++ packages/harmonyc/src/processor/autoLabel.ts
@@ -1,15 +1,16 @@
 import { Branch, Label, Phrase, Section, Step } from '../model/model'
 
 export function autoLabel(s: Branch) {
-  if (s.children.length <= 1) return
-  s.children
-    .filter((c) => c instanceof Step)
-    .filter((c) => c.isFork)
-    .forEach((c) => {
-      const label = c.action.toSingleLineString()
-      const autoSection = new Section(new Label(label), [], c.isFork)
-      c.replaceWith(autoSection)
-      autoSection.addChild(c)
-    })
+  const forks = s.children.filter((c, i) => c.isFork || i === 0)
+  if (forks.length > 1) {
+    forks
+      .filter((c) => c instanceof Step)
+      .forEach((c) => {
+        const label = c.action.toSingleLineString()
+        const autoSection = new Section(new Label(label), [], c.isFork)
+        c.replaceWith(autoSection)
+        autoSection.addChild(c)
+      })
+  }
   s.children.forEach((c) => autoLabel(c))
 }
