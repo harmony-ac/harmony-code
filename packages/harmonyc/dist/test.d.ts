a7ece3189e8444e41f4c3fc5d0d9177661e257ce
Bernát Kalló <kallo.bernat@gmail.com>
2025-03-23 15:18:43 +0100

    types: add vitest.d.ts

--- packages/harmonyc/dist/test.d.ts
+++
@@ -1,7 +0,0 @@
-type FeatureContext = {
-  Action(s: string): Promise<void>
-  Action(s: string, fn: Function): void
-  Response(s: string): Promise<void>
-  Response(s: string, fn: Function): void
-}
-export function Feature(name: string, fn: (ctx: FeatureContext) => void): void
