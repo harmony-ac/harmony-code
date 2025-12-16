b2137e53a05d187e2483bbd95b0ac84304778ecd
Bernát Kalló <kallo.bernat@gmail.com>
2025-12-16 13:43:58 +0100

    tests: configure timeout

--- packages/harmonyc/vitest.config.ts
+++ packages/harmonyc/vitest.config.ts
@@ -4,6 +4,7 @@ import harmonyPlugin from './src/vitest/index.ts'
 export default defineConfig({
   plugins: [harmonyPlugin({})],
   test: {
+    testTimeout: 15_000,
     include: ['src/**/*.harmony'],
     forceRerunTriggers: ['vitest.config.ts', 'src/vitest/index.ts'],
   },
